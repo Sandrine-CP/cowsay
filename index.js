3e992b06541a3a505cd19f18302e09bca2f7fad6
MacBook Pro Sandrine CP <macbookprosandrinecp@MacBook-Pro-de-MacBook.local>
2024-06-11 11:20:32 +0200

    syntaxe changes on both index.js and information.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const wild = require("./information");
-console.log(`Hi, my name is ${wild.name} and I'm in a ${wild.campus} campus`);
-
+// console.log(`Hi, my name is ${wild.name} and I'm in a ${wild.campus} campus`);
 
 const cowsay = require("cowsay");
 
 console.log(cowsay.say({
-    text : "I'm a moooodule",
+    text : `Hi, my name is ${wild.name} and I'm in a ${wild.campus} campus`,
     e : "oO",
     T : "U",
 }));
--- information.js
+++ information.js
@@ -1,10 +1,7 @@
 const wild = {
     name:"Cazenave",
     campus:"Remote",
-}
-console.log(`My name is ${wild.name} and I am on ${wild.campus} campus`);
-
-module.exports = {
-    name: wild.name,
-    campus: wild.campus,
 };
+// console.log(`My name is ${wild.name} and I am on ${wild.campus} campus`);
+
+module.exports = wild;
